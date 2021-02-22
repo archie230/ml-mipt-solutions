7401f8e5bc89a688ebd5485c44290e5b7e5e9753
archie230 <mulikhov.am@phystech.edu>
2021-02-22 15:15:04 +0300

    [adv] hw2 progress

--- homeworks_advanced/assignment1_02_Three_headed_network/network.py
+++ homeworks_advanced/assignment1_02_Three_headed_network/network.py
@@ -1,4 +1,4 @@
-
+from collections import OrderedDict
 import numpy as np
 import pandas as pd
 
@@ -13,29 +13,42 @@ class ThreeInputsNet(nn.Module):
     def __init__(self, n_tokens, n_cat_features, concat_number_of_features, hid_size=64):
         super(ThreeInputsNet, self).__init__()
         self.title_emb = nn.Embedding(n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>        
+        self.title_conv_pooling = nn.Sequential(OrderedDict([
+            ('title_conv1', nn.Conv1d(
+                in_channels=hid_size,
+                out_channels=hid_size,
+                kernel_size=2)),
+            ('title_relu1', nn.ReLU()),
+            ('title_adapt_avg_pool', nn.AdaptiveAvgPool1d(output_size=1))
+        ]))
         
         self.full_emb = nn.Embedding(num_embeddings=n_tokens, embedding_dim=hid_size)
-        # <YOUR CODE HERE>
+        self.full_conv_pooling = nn.Sequential(OrderedDict([
+            ('full_conv1', nn.Conv1d(
+                in_channels=hid_size,
+                out_channels=hid_size,
+                kernel_size=2)),
+            ('full_relu1', nn.ReLU()),
+            ('full_adapt_avg_pool', nn.AdaptiveAvgPool1d(output_size=1))
+        ]))
         
-        self.category_out = # <YOUR CODE HERE>
-
+        self.category_out = nn.Linear(in_features=n_cat_features, out_features=hid_size)
 
         # Example for the final layers (after the concatenation)
+        self.bn = nn.BatchNorm1d(concat_number_of_features)
         self.inter_dense = nn.Linear(in_features=concat_number_of_features, out_features=hid_size*2)
         self.final_dense = nn.Linear(in_features=hid_size*2, out_features=1)
-
         
 
     def forward(self, whole_input):
         input1, input2, input3 = whole_input
         title_beg = self.title_emb(input1).permute((0, 2, 1))
-        title = # <YOUR CODE HERE>
+        title = self.title_conv_pooling(title_beg) # <YOUR CODE HERE>
         
         full_beg = self.full_emb(input2).permute((0, 2, 1))
-        full = # <YOUR CODE HERE>        
+        full = self.full_conv_pooling(full_beg) # <YOUR CODE HERE>        
         
-        category = # <YOUR CODE HERE>        
+        category = self.category_out(input3) # <YOUR CODE HERE>        
         
         concatenated = torch.cat(
             [
@@ -45,6 +58,8 @@ class ThreeInputsNet(nn.Module):
             ],
             dim=1)
         
-        out = # <YOUR CODE HERE>
+        out = self.inter_dense(self.bn(concatenated))
+        out = self.final_dense(out)
+        # <YOUR CODE HERE>
         
         return out
